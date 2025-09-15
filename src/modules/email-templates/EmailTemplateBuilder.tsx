240bb81875d2ee5f7b0a9fa2a7d082e893324d1a
Jacky <tdhungit@gmail.com>
2025-09-15 11:58:36 +0700

    refactor(email-templates): Improve builder UI and remove unused components
    This commit refactors the email template builder to enhance the user interface and remove unused components.
    
    - Removed CustomAssetManager and CustomModal components.
    - Improved StylePropertyField with better input rendering and styling.
    - Updated CustomTraitManager to include StylesProvider.

--- src/modules/email-templates/EmailTemplateBuilder.tsx
+++ src/modules/email-templates/EmailTemplateBuilder.tsx
@@ -1,12 +1,5 @@
-import GjsEditor, {
-  AssetsProvider,
-  Canvas,
-  ModalProvider,
-  TraitsProvider,
-} from '@grapesjs/react';
+import GjsEditor, { Canvas, TraitsProvider } from '@grapesjs/react';
 import type { Editor, EditorConfig } from 'grapesjs';
-import CustomAssetManager from './components/builders/CustomAssetManager';
-import CustomModal from './components/builders/CustomModal';
 import CustomTraitManager from './components/builders/CustomTraitManager';
 import RightSidebar from './components/builders/RightSidebar';
 import TopBar from './components/builders/TopBar';
@@ -19,13 +12,6 @@ const gjsOptions: EditorConfig = {
   undoManager: { trackSelection: false },
   selectorManager: { componentFirst: true },
   projectData: {
-    assets: [
-      'https://via.placeholder.com/350x250/78c5d6/fff',
-      'https://via.placeholder.com/350x250/459ba8/fff',
-      'https://via.placeholder.com/350x250/79c267/fff',
-      'https://via.placeholder.com/350x250/c5d647/fff',
-      'https://via.placeholder.com/350x250/f28c33/fff',
-    ],
     pages: [
       {
         name: 'Home page',
@@ -82,25 +68,6 @@ export default function EmailTemplateBuilder() {
           </TraitsProvider>
         </div>
       </div>
-
-      <ModalProvider>
-        {({ open, title, content, close }) => (
-          <CustomModal
-            open={open}
-            title={title}
-            children={content}
-            close={close}
-          />
-        )}
-      </ModalProvider>
-
-      <AssetsProvider>
-        {({ assets, select, close, Container }) => (
-          <Container>
-            <CustomAssetManager assets={assets} select={select} close={close} />
-          </Container>
-        )}
-      </AssetsProvider>
     </GjsEditor>
   );
 }
